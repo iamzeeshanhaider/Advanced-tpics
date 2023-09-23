11eef96997442d7308c48122f28826c2e5d61986
iamzeeshanhaider <zeeshansupari001@gmail.com>
2023-09-23 20:45:24 +0500

    first commit

---
+++ database/seeders/ScheduledClassSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ScheduledClass;
+
+class ScheduledClassSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ScheduledClass::factory()->count(10)->create();
+    }
+}
